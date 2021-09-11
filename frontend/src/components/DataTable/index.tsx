9bbba952afd3a593ee7a8230c2c8f4015fb88d0a
Alexandre Costa <alex.o.costa@gmail.com>
2021-09-11 19:44:07 -0300

    DataTable integration

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,4 +1,28 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
+import { SalePage } from "types/sale";
+import { formatLocalDate } from "utils/format";
+import { BASE_URL } from "utils/requests";
+
 const DataTable = () => {
+
+    const [page, setPage] = useState<SalePage>({
+        last: true,
+        totalPages: 0,
+        totalElements: 0,
+        number: 0,
+        first: true
+    });
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales?page=1&size=20&sort=date,desc`)
+            .then((response) => {
+                setPage(response.data);
+            });
+    }, []);
+
+    /*
     return (
         <div className="table-responsive">
             <table className="table table-striped table-sm">
@@ -82,6 +106,34 @@ const DataTable = () => {
                         <td>25</td>
                         <td>15017.00</td>
                     </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+    */
+
+    return (
+        <div className="table-responsive">
+            <table className="table table-striped table-sm">
+                <thead>
+                    <tr>
+                        <th>Data</th>
+                        <th>Vendedor</th>
+                        <th>Clientes visitados</th>
+                        <th>Negócios fechados</th>
+                        <th>Valor</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {page.content?.map(item => (
+                        <tr key={item.id}>
+                            <td>{formatLocalDate(item.date, "dd/MM/yyyy")}</td>
+                            <td>{item.seller.name}</td>
+                            <td>{item.visited}</td>
+                            <td>{item.deals}</td>
+                            <td>{item.amount.toFixed(2)}</td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
         </div>
